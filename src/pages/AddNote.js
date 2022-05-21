1e6e450a64e30db2f8cfbbdee30c2a89515f7e36
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-21 17:37:34 +0530

    note adding page made

--- src/pages/AddNote.js
+++ src/pages/AddNote.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from "../components/Form";
 
 const AddNote = () => {
-	return <div>AddNote</div>;
+	const [input, setInput] = useState({
+		title: "",
+		content: ""
+	});
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		
+
+	};
+
+	const handleChange = (name) => (event) => {
+		setInput({...input, [name]: event })
+	}
+
+	const
+
+	return (
+		<div>
+			<Form handleSubmit={handleSubmit}/>
+		</div>
+	);
 };
 
 export default AddNote;
