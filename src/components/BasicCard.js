98ec68574f9b9f728d5c2d9e155c554c6e697514
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-22 09:44:33 +0530

    Card component added

---
+++ src/components/BasicCard.js
@@ -0,0 +1,30 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const BasicCard = (props) => {
+	return (
+		<Card sx={{ minWidth: 275 }}>
+			<CardContent>
+				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+					{props.title}
+				</Typography>
+				<Typography sx={{ mb: 1.5 }} color="text.secondary">
+					{props.date}
+				</Typography>
+				<Typography variant="body2">{props.content}</Typography>
+			</CardContent>
+			<CardActions>
+				<IconButton aria-label="delete-note" size="small">
+					<DeleteIcon fontSize="inherit" />
+				</IconButton>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default BasicCard;
