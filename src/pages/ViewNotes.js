81166c4a287ff9ea5b85b86a2fdaabc5b5d67014
anapeksha <anapeksha.mukherjee@gmail.com>
2022-05-22 21:53:27 +0530

    view note cards

--- src/pages/ViewNotes.js
+++ src/pages/ViewNotes.js
@@ -1,25 +1,49 @@
+import { Box, Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import BasicCard from "../components/BasicCard";
 import getData from "../controllers/getData";
+import "../styles/view-notes.css";
 
 const ViewNotes = () => {
 	const [notes, setNotes] = useState([]);
 
 	useEffect(() => {
-		const data = getData();
-		setNotes(data);
+		getData.then((data) => {
+			setNotes(data);
+		});
 	}, []);
+
+	const notesDisplay = () => {
+		if (notes !== null) {
+			return notes.map((note) => {
+				return (
+					<Grid item xs={6} sm={6} md={6}>
+						<BasicCard
+							title={note.title}
+							date={note.date}
+							content={note.content}
+						/>
+					</Grid>
+				);
+			});
+		}
+	};
 
 	return (
 		<div className="view-notes-body">
-			{notes &&
-				notes.map((note) => {
-					<BasicCard
-						title={note.title}
-						date={note.title}
-						content={note.content}
-					/>;
-				})}
+			<Box
+				sx={{
+					height: "75vh",
+					overflow: "auto",
+					borderRadius: "5px",
+					margin: "5vw 5vh",
+				}}
+				className="animate__animated animate__fadeIn animate__slower"
+			>
+				<Grid container spacing={0.75}>
+					{notesDisplay()}
+				</Grid>
+			</Box>
 		</div>
 	);
 };
